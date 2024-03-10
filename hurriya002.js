e5e18838c1957f27aedf8730df32e2831ac55700
Dildara10 <162028778+Dildara10@users.noreply.github.com>
2024-03-10 22:41:28 +0500

    Add files via upload

---
+++ hurriya002.js
@@ -0,0 +1,19 @@
+var percentAge = 45;
+if (percentAge > 90) {
+    console.log("Grad A+");
+}
+else if (percentAge > 80) {
+    console.log("Grad A");
+}
+else if (percentAge > 70) {
+    console.log("Grad B");
+}
+else if (percentAge > 60) {
+    console.log("Grad C");
+}
+else if (percentAge > 50) {
+    console.log("Grad D");
+}
+else {
+    console.log("Fail");
+}
